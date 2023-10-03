18fb2964631573755a1ccec19f48162c97942074
lorraine-quantum <120484389+lorraine-quantum@users.noreply.github.com>
2023-10-03 17:29:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ const cloud = process.env.CLOUD_URI;
 // })
 const start = async () => {
   try {
-    await connectDB(local);
+    await connectDB(cloud);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
